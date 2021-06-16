7cf4efb1014f8d3e14105bef464750c7e6b465f8
souzavini <viniciuscarr@hotmail.com>
2021-06-15 22:23:56 -0300

    Implementar fake de requisiçoes para API e configurando Axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,35 +1,43 @@
-import {Container} from './styles'
+import { useEffect } from 'react';
+import { api } from '../../services/api';
+import { Container } from './styles'
 
-export function TransactionsTable(){
-return(
-    <Container>
-        <table>
-            <thead>
-                <tr>
-                    <th>Titulo</th>
-                    <th>Valor</th>
-                    <th>Categoria</th>
-                    <th>Data</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Desenvolvimento web</td>
-                    <td className="deposit">R$ 12.000</td>
-                    <td>Desenvolvimento</td>
-                    <td>20/01/2012</td>
-                </tr>
+export function TransactionsTable() {
+    useEffect(() => {
+        api.get('transactions')
+            .then(response => console.log(response.data))
+    }, [])
 
-                <tr>
-                    <td>paguei</td>
-                    <td className="withdraw">R$ -12.000</td>
-                    <td>Desenvolvimento</td>
-                    <td>20/01/2012</td>
-                </tr>
-            </tbody>
 
-        </table>
-    </Container>
-)
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Titulo</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento web</td>
+                        <td className="deposit">R$ 12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/01/2012</td>
+                    </tr>
+
+                    <tr>
+                        <td>paguei</td>
+                        <td className="withdraw">R$ -12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/01/2012</td>
+                    </tr>
+                </tbody>
+
+            </table>
+        </Container>
+    )
 
 }
