ac60e0261bcafd9fd7595d779f5c71df6609507a
souzavini <viniciuscarr@hotmail.com>
2021-06-18 08:51:45 -0300

    Implementacao do Context e Hooks

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Modal from 'react-modal';
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./hooks/useTransactions";
+
 
 Modal.setAppElement('#root')
 
@@ -20,11 +22,11 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
       <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
